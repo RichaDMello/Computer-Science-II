ac2247b695b05cb26b8b2cfca263ccf1397fb8c9
Richa D'Mello <92398790+RichaDMello@users.noreply.github.com>
2022-10-04 12:56:47 -0400

    Update ArraySum.java

--- RecursionLab/ArraySum.java
+++ RecursionLab/ArraySum.java
@@ -1,25 +1,14 @@
-
 public class ArraySum {
 	
 	public int sumOfArray (Integer[] a,int index)
 	{
-		//base case = when index = end of array or end of entries
-		//original problem = a[index++]
+		//base case = when index = 0
+		//original problem = a[index--]
 		//reduced problem = sumOfArray(a, index)
 		
-		int Sum;
-		
-		if (index < a.length)
+		if (index < a.length && index > -1)
 		{
-			if (a[index] != null)
-			{
-				Sum = a[index++];
-				return Sum + sumOfArray(a, index);
-			}
-			else
-			{
-				return 0;
-			}
+			return a[index] + sumOfArray(a, --index);
 		}
 		else
 		{
@@ -27,3 +16,4 @@ public class ArraySum {
 		}
 	}
 }
+
